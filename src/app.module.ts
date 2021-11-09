03be7ff5b311d31850c706d7888282fe22d7159d
ys <pyyyyysv@gmail.com>
2021-11-10 01:13:04 +0900

    Add local env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TradeModule } from './trade/trade.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_DB, {
       useNewUrlParser: true,
     }),
